ea30147a76d9a0916e2dd7fb3c6a1005e4839830
alice <alice.caron@telecom-paristech.fr>
2013-06-18 10:13:40 +0200

    Debut comptage des mots

---
+++ eclipse/workspace/.metadata/.plugins/org.eclipse.core.resources/.history/da/d088e9d4ead700121b01c6c6b6b1e763
@@ -0,0 +1,18 @@
+package analyse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Words {
+	private int corpusFrequency;
+	private String word;
+	private String type;
+	private Map<String,DocInfo> docPresence = new HashMap<String,DocInfo>();
+	
+	public Words(String word,String type){
+		this.corpusFrequency=1;
+		this.word=word;
+		this.type=type;
+
+	}
+}
--- eclipse/workspace/paf/src/analyse/Comptage.java
+++ eclipse/workspace/paf/src/analyse/Comptage.java
@@ -2,15 +2,19 @@ package analyse;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 public class Comptage {
-	private Set<Words> corpusWords=new HashSet<Words>();
+	private static Map<String,Words> corpusWords=new HashMap<String,Words>();
 	private static Set<File> corpus=new HashSet<File>();
 //	private Map<String,File> corpusFileNames=new HashMap<String,File>();
 
@@ -20,13 +24,50 @@ public class Comptage {
 		getRepositoryList(repository);
 		for(File file:corpus)
 			computeDocument(file);
+		displayStatistics();
 	}
 	
+	private static void displayStatistics() {
+		Set<String> clef = corpusWords.keySet();
+		Iterator<String> it = clef.iterator();
+		while (it.hasNext()){
+		   String key =  it.next();
+		   Words word = corpusWords.get(key);
+		   System.out.println(key+" apparaît "+word.getCorpusFrequency()+" fois dans ce corpus de textes.");
+		}
+		
+	}
+
 	private static void computeDocument(File file) throws IOException {
 		String content = getFileContent(file);
-		String[] wordsTab = content.split("([ \t\n\r\f.\\'])");
-		for(String word:wordsTab)
-			System.out.println(word);
+		//System.out.println(content);
+		content=StringEscapeUtils.escapeJava(content);
+		System.out.println(content);
+		content = content.replaceAll("([ \t\n\r\f.'():]|u2019|,)","%%%");
+		System.out.println("here "+content);
+		content=StringEscapeUtils.unescapeJava(content);
+		String[] wordsTab=content.split("%%%");
+		
+		for(String word:wordsTab){
+			if(!word.equals("") && !(word.length()<2)){
+				System.out.println(word);
+				computeWord(word,file.getAbsolutePath());
+			}
+		}
+	}
+
+	private static void computeWord(String str,String documentPath) {
+		String[] wordProp = str.split("_");
+		Words lastWord;
+		String theWord=wordProp[0];
+		if(!corpusWords.containsKey(theWord)){
+			if(wordProp.length==2)
+				lastWord = new Words(theWord,wordProp[1],documentPath);	
+		}
+		else{
+			corpusWords.get(theWord).updateCorpusFrequency();
+			corpusWords.get(theWord).updateDocumentFrequency(documentPath);
+		}	
 	}
 
 	private static String getFileContent(File file) throws IOException {
@@ -35,7 +76,7 @@ public class Comptage {
 		String line;
 		String content=null;
 		while((line=buffer.readLine())!=null)
-			content+=line;
+			content+=" "+line;
 		return content;
 	}
 
